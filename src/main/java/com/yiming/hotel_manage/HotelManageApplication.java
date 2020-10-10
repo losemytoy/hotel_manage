c97d33ea9fa613b3cfa4b2f38d592d80db09588a
2391660155@qq.com <2391660155@qq.com>
2020-10-10 18:02:23 +0800

    Initial commit

---
+++ src/main/java/com/yiming/hotel_manage/HotelManageApplication.java
@@ -0,0 +1,13 @@
+package com.yiming.hotel_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManageApplication.class, args);
+    }
+
+}
